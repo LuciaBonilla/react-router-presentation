f23006729fb03ae7fa716034b3af8c1dc70d1e3e
mathiassellanes <mathias@neocoast.com>
2024-09-16 17:41:54 -0300

    fixes

--- src/routes/NavigationTypes/texts.jsx
+++ src/routes/NavigationTypes/texts.jsx
@@ -1,43 +1,43 @@
 export const navigate = `
-<input 
-  id="inputRouteToNavigate" 
-  type="text" 
-  placeholder="Enter some text" 
-  value={inputValue} 
-  onChange={(e) => setInputValue(e.target.value)} 
+<input
+  id="inputRouteToNavigate"
+  type="text"
+  placeholder="Enter some text"
+  value={inputValue}
+  onChange={(e) => setInputValue(e.target.value)}
 />
 <Navigate to={\`/navigationTypes/navigate/\${inputValue}\`} replace={true} />
 `;
 
 export const link = `<Link to='/navigationTypes/link/1' className="button">
-            Dirección 1
-        </Link>
+    Dirección 1
+</Link>
 
-        <Link to='/navigationTypes/link/2' className="button">
-            Dirección 2
-        </Link>
+<Link to='/navigationTypes/link/2' className="button">
+    Dirección 2
+</Link>
 
-        <Link to='/navigationTypes/link/3' className="button">
-            Dirección 3
-        </Link>`
+<Link to='/navigationTypes/link/3' className="button">
+    Dirección 3
+</Link>`
 
 export const navLink = `
-<NavLink 
-  to='/navigationTypes/navLink/1' 
+<NavLink
+  to='/navigationTypes/navLink/1'
   className={({ isActive }) => \`\${isActive ? 'bigButton' : 'button'}\`}
 >
   Dirección 1
 </NavLink>
 
-<NavLink 
-  to='/navigationTypes/navLink/2' 
+<NavLink
+  to='/navigationTypes/navLink/2'
   className={({ isActive }) => \`\${isActive ? 'bigButton' : 'button'}\`}
 >
   Dirección 2
 </NavLink>
 
-<NavLink 
-  to='/navigationTypes/navLink/3' 
+<NavLink
+  to='/navigationTypes/navLink/3'
   className={({ isActive }) => \`\${isActive ? 'bigButton' : 'button'}\`}
 >
   Dirección 3
@@ -66,7 +66,7 @@ const UseNavigateExplanation = () => {
 
 export const useLocationCode = `
 const UseLocationExplanation = () => {
-    const location = useLocation(); 
+    const location = useLocation();
 
   return (
     <div>
--- src/routes/Routes/UseSearchParams/index.jsx
+++ src/routes/Routes/UseSearchParams/index.jsx
@@ -1,28 +1,39 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import { Outlet } from "react-router-dom";
-import { routestext } from "../texts";
 import { dracula } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { useSearchParams } from "react-router-dom";
 
+import { routestext } from "../texts";
 
+const UseSearchParams = () => {
+    const [searchParams, setSearchParams] = useSearchParams();
+    const [inputValue, setInputValue] = useState(searchParams.get('search') || '');
 
-const useSearchParams = () => {
+    const handleChange = (e) => {
+        setInputValue(e.target.value);
+        setSearchParams({ search: e.target.value });
+    }
+
     return (
         <div>
             <h1>useSearchParams</h1>
             <p>El hook <code>useSearchParams</code> se utiliza para obtener los parámetros de la URL.</p>
             <h3>Ejemplo Basico</h3>
+
+            <input type="text" value={inputValue} onChange={handleChange} />
+
+            <h2>Los parametros de la URL son:</h2>
+            <p>{searchParams.get('search')}</p>
+
             <SyntaxHighlighter showLineNumbers language="jsx" style={dracula}>
                 {routestext}
             </SyntaxHighlighter>
-            <Link to='/thanks' className = "button"> 
+            <Link to='/thanks' className = "button">
                 Siguiente
             </Link>
         </div>
     );
-
-
-
 }
 
-export default useSearchParams;
+export default UseSearchParams;
